485b9ae6e18d4984a42980889f25336c7f198ee6
Mike Chambers <mikechambers@gmail.com>
2020-11-12 15:26:53 -0800

    Lots of clean up and house keeping.

--- dclis/src/apiclient.rs
+++ dclis/src/apiclient.rs
@@ -1,3 +1,25 @@
+/*
+* Copyright 2020 Mike Chambers
+* https://github.com/mikechambers/dcli
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of 
+* this software and associated documentation files (the "Software"), to deal in 
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is furnished to do 
+* so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all 
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 use reqwest::Url;
 
 const DESTINY_API_KEY: &str = env!("DESTINY_API_KEY");
@@ -13,18 +35,20 @@ pub enum ApiCallErrorType {
 }
 
 pub struct ApiClient {
-    //TODO: add verbose / print URL
+    pub print_url:bool,
 }
 
 impl ApiClient {
-    pub fn new() -> ApiClient {
-        ApiClient {}
+    pub fn new(print_url:bool) -> ApiClient {
+        ApiClient {print_url}
     }
 
     pub async fn call_api(&self, url: String) -> Result<reqwest::Response, reqwest::Error> {
         let url = Url::parse(&url).unwrap();
 
-        println!("{}", url);
+        if self.print_url {
+            println!("{}", url);
+        }
 
         let client = reqwest::Client::new();
 
--- dclis/src/memberidsearch.rs
+++ dclis/src/memberidsearch.rs
@@ -1,3 +1,25 @@
+/*
+* Copyright 2020 Mike Chambers
+* https://github.com/mikechambers/dcli
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of 
+* this software and associated documentation files (the "Software"), to deal in 
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is furnished to do 
+* so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all 
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 use crate::apiclient::{ApiCallError, ApiCallErrorType, ApiClient};
 use crate::platform::Platform;
 
@@ -6,15 +28,13 @@ use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
 use serde_derive::{Deserialize, Serialize};
 
 pub struct MemberIdSearch {
-    print_url: bool,
     client: ApiClient,
 }
 
 impl MemberIdSearch {
     pub fn new(print_url: bool) -> MemberIdSearch {
         MemberIdSearch {
-            print_url,
-            client: ApiClient::new(),
+            client: ApiClient::new(print_url),
         }
     }
 
