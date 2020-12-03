e2f3c7c28671b2065d5dec9b95375bc7aadd5698
Mike Chambers <mikechambers@gmail.com>
2020-12-03 13:13:17 -0800

    Make script a bit more robust
    -defaults to not check status if destiny is not running
    -doesn't show notification when script first launches
    -after status changes, delays next check for one minute (so there isnt bounce in api results)

--- examples/status_notification_win.ps1
+++ examples/status_notification_win.ps1
@@ -13,7 +13,17 @@
 # https://github.com/mikechambers/dcli/
 
 
-$check_interval_seconds=15
+
+################ Script configuration #################
+$default_check_interval_seconds=15
+
+#only check if Destiny process is running. Set to $false
+#if you want it to check regardless of whether Destiny is running
+#on the same machine
+$only_check_if_destiny_running = $true
+
+#whether it should print out status and other output to console
+$quiet=$false
 
 $member_id="00000000000000000000"
 $platform="xbox"
@@ -22,28 +32,62 @@ $platform="xbox"
 #to sync manifest before running this script
 $manifest_path="/tmp/manifest.sqlite3"
 
+############# program #############
+
 $old_activity=""
 
 while ($true) {
 
+    $check_interval_seconds = $default_check_interval_seconds
+    #check if Destiny is running and whether we should skip check if its not running
+    $should_check = (Get-Process destiny2 -ErrorAction SilentlyContinue) -or !$only_check_if_destiny_running
 
-    $activity = (dclia --manifest-path $manifest_path --member-id $member_id --platform $platform) -join "`n"
 
-    #note the inconsistent use of trailing period. Ill fix this in the app
-    $skip_notification = (($activity -eq "Not currently in an activity.") -or ($activity -eq "Currently sitting in Orbit"))
-    if ( ($old_activity -ne $activity) -and  !$skip_notification)
-    { 
-        [reflection.assembly]::loadwithpartialname("System.Windows.Forms")
-        [reflection.assembly]::loadwithpartialname("System.Drawing")
-        $notify = new-object system.windows.forms.notifyicon
-        $notify.icon = [System.Drawing.SystemIcons]::Information
-        $notify.visible = $true
-        $notify.showballoontip(10,"Destiny 2 Activity Changed",$activity,[system.windows.forms.tooltipicon]::None)
+    if ($should_check) {
+        if (!$quiet) {
+            Write-Output "Checking Status..."
+        }
 
-        $old_activity = $activity
+        # assumes dclia is in your path
+        $activity = (dclia --manifest-path $manifest_path --member-id $member_id --platform $platform) -join "`n"
+        #note the inconsistent use of trailing period. Ill fix this in the app
+        $skip_notification = (($activity -eq "Not currently in an activity.") -or ($activity -eq "Currently sitting in Orbit"))
+    
+        #dont send notification the first time we run
+        if($old_activity -eq "") {
 
-        #TODO: when we find a new activity, lets delay the next check for a bit, in case the server api returns old data
-        # for a short period
+            if (!$quiet) {
+                Write-Output $activity
+                Write-Output "Initial status check. Skipping notification."
+            }
+
+            $skip_notification = $true
+            $old_activity = $activity
+        }
+
+        if ( ($old_activity -ne $activity) -and  !$skip_notification)
+        { 
+            [void] [reflection.assembly]::loadwithpartialname("System.Windows.Forms")
+            [void] [reflection.assembly]::loadwithpartialname("System.Drawing")
+            $notify = new-object system.windows.forms.notifyicon
+            $notify.icon = [System.Drawing.SystemIcons]::Information
+            $notify.visible = $true
+            $notify.showballoontip(10,"Destiny 2 Activity Changed",$activity,[system.windows.forms.tooltipicon]::None)
+    
+            if (!$quiet) {
+                Write-Output $activity
+            }
+
+            $old_activity = $activity
+    
+            #TODO: when we find a new activity, lets delay the next check for a bit, in case the server api returns old data
+            # for a short period
+            $check_interval_seconds = 60
+        }
+    } else {
+        if (!$quiet) {
+            Write-Output "Destiny 2 is not running. Skipping status check."
+        }
     }
 
     Start-Sleep -Seconds $check_interval_seconds
