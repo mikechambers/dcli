331f154629237a41e58ea330cdba2c5ac3feaff3
Mike Chambers <mesh@pop-os.localdomain>
2020-12-08 22:23:39 -0800

    StartMoment::get_date_time should not be called for Custom.
    Right now, I just return Unix epoch, but may consider putting an assert
    here.

--- src/dcliah/src/startmoment.rs
+++ src/dcliah/src/startmoment.rs
@@ -24,6 +24,8 @@
 use std::fmt;
 use std::str::FromStr;
 use chrono::{DateTime, Utc, Duration};
+use chrono::prelude::*;
+
 use dcli::utils::{get_last_weekly_reset, get_last_daily_reset, get_last_friday_reset};
 
 //TODO: sync these with dclitime
@@ -63,8 +65,11 @@ impl StartMoment {
             StartMoment::AllTime => {
                 Utc::now() - Duration::weeks(52 * 7)
             },
+            //The entire point of custom is to let user specify they will enter
+            //their own value, so get_date_time should not be called for custom/
+            //TODO: could assert here to enforce it
             StartMoment::Custom => {
-                Utc::now() //TODO: change this to unixtimestamp
+                Utc.ymd(0, 0, 0).and_hms(0, 0, 0) //TODO: change this to unixtimestamp
             },
 
         }
