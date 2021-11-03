6db8fd074581a230686bcef318de4d064897156c
Mike Chambers <mikechambers@gmail.com>
2021-11-02 20:16:04 -0700

    Fixed a formatting issue with medals

--- src/dcliah/src/main.rs
+++ src/dcliah/src/main.rs
@@ -484,7 +484,7 @@ fn print_default(
         let col_w_w = col_w + 2;
         let col_w_h = col_w / 2;
         let med_header_str = format!(
-        "{:<0map_col_w$}{:<col_w_h$}{:>0col_w$}{:>0col_w$}{:>0col_w$}{:>col_w_h$}{:>0col_w$}",
+        "{:<0map_col_w$}{:<col_w_h$}{:>0col_w$}{:>0col_w$}{:>0col_w$}{:>col_w_h$}{:<0col_w$}",
         "MEDAL",
         "",
         "COUNT",
@@ -532,18 +532,18 @@ fn print_default(
                 let gm = (activity_count as f32 / m.count as f32).ceil();
 
                 println!(
-            "{:<0map_col_w$}{:<col_w_h$}{:>0col_w$}{:>0col_w$}{:>0col_w$}{:>col_w_h$}{:>0col_w$}",
-            m.medal.name,
-            gold,
-            m.count.to_string(),
-            format_f32(m.count as f32 / activity_count as f32, 2),
-            format!("{}", gm),
-            "",
-            truncate_ascii_string(&m.medal.description, med_divider_len - (wep_col + (col_w_w * 3) + col_w_h * 2)),
-            col_w = col_w_w,
-            map_col_w = wep_col,
-            col_w_h = col_w_h
-        );
+                    "{:<0map_col_w$}{:<col_w_h$}{:>0col_w$}{:>0col_w$}{:>0col_w$}{:>col_w_h$}{:<0col_w$}",
+                    m.medal.name,
+                    gold,
+                    m.count.to_string(),
+                    format_f32(m.count as f32 / activity_count as f32, 2),
+                    gm.to_string(),
+                    "",
+                    truncate_ascii_string(&m.medal.description, med_divider_len - (wep_col + (col_w_w * 3) + col_w_h * 2)),
+                    col_w = col_w_w,
+                    map_col_w = wep_col,
+                    col_w_h = col_w_h
+                );
             }
         } else {
             println!("No medals");
